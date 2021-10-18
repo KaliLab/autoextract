f6a32e340b3d30e273972ac2c20e30e363d10d77
blazma <martin.blazsek@gmail.com>
2021-10-18 17:54:09 +0200

    Create automated feature extraction script

---
+++ README.md
@@ -0,0 +1,55 @@
+﻿## Automated feature extraction using BluePyEfe
+
+This script scans through an input directory (organized in a certain way), find cells and their respective measurements, group cells by protocol and run a BPE feature extraction for all cells belonging to the same protocol. 
+
+To run this script the following components are required:
+ - **input directory** contianing data organized in a particular structure
+ - **protocol.txt** files for each measurement
+ - **config.json**
+ - **output directory**: BPE output will be saved here
+
+### Input data - directory structure
+Inside the input directory following structure is assumed by the script: each cell has a subdirectory under the input directory. Each cell directory has one or more measurement subdirectories. Each measurement subdirectory has measurement files and a protocol.txt that provides the protocol for a given measurement as well as other information. Summary:
+
+    input_dir
+	    cell1_dir
+		    meas1_dir
+			    - meas1_ch1_file
+			    - meas1_ch2_file
+			    - protocol.txt
+		    meas2_dir
+		    ...
+	    cell2_dir
+	    ...
+	   
+### Protocol files
+These protocol.txt files either have 5 or 6 rows containing the following information in each row:
+
+ - **protocol name**: unique identifier of protocol; used for grouping
+ - **channel**: which file contains the voltage data; this string is searched for in the measurement filenames)
+ - **sampling rate** (Hz)
+ - **parameter list**: a 3-tuple containing (ton, toff, duration) - needed for BPE
+ - **amplitudes list**: list of current injection amplitudes (pA)
+ - **missing sweeps list**: list of indices of amplitudes for which the measurements are deemed to be unused for some reason
+
+### Config JSON
+The script requires a config.json that contains further (protocol-unspecific) configuration parameters that are required to run BluePyEfe (e.g.: list of features to be extracted).
+
+### How to run the script
+The script takes three parameters:
+
+    -cp / --config-path: Path to configuration JSON
+    -ip / --input-path: Root path for input directory containing raw electrophysiological data
+    -op / --output-path: Directory where BPE feature extraction results should be saved
+
+   Example run:
+   
+
+    python extract.py -cp /home/martin/koki/extract/config.json -ip /home/martin/koki/extract/minta_konyvtar -op /home/martin/koki/extract/feature_extract_output
+
+To run the script you need to have BluePyEfe installed. If you want to collect global features (e.g.: rheobase current and all features grouped by rheobase current, fI-curve slopes, etc.) please install the following, modified version of BPE: https://github.com/blazma/BluePyEfe (dev branch)
+
+
+
+
+
---
+++ extract.py
@@ -0,0 +1,194 @@
+import os
+import sys
+import json
+import logging
+import argparse
+import bluepyefe as bpefe
+
+
+###### HELPER FUNCTIONS ######
+def get_subdirectory_list(path_str):
+    directory_walk = list(os.walk(path_str))  # list of all subdirectories + files under path_str
+    directory_root_tuple = directory_walk[0]  # select only first directory, i.e the path_str root
+    subdirectories = directory_root_tuple[1]  # (root, dirs, files) tuple -> select dirs (list)
+    return subdirectories
+
+
+def filter_measurements_by_protocol(measurement_dict, protocol_type):
+    filtered_measurement_dict = {}
+    for cell in measurement_dict:
+        for measurement in measurement_dict[cell]:
+            if measurement_dict[cell][measurement]['protocol_type'] == protocol_type:
+                if cell not in filtered_measurement_dict:
+                    filtered_measurement_dict[cell] = {}
+                filtered_measurement_dict[cell][measurement] = measurement_dict[cell][measurement]
+    return filtered_measurement_dict
+
+
+def print_protocol_type(protocol_type):
+    # just because it's pretty
+    print("#" * 5 + "#" * (len(protocol_type) + 2) + "#" * 5)
+    print("#" * 5 + " {} ".format(protocol_type) + "#" * 5)
+    print("#" * 5 + "#" * (len(protocol_type) + 2) + "#" * 5)
+##############################
+
+# save console output to log file
+sys.stdout = open('extract_log.txt', 'w')
+
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-cp', '--config-path', type=str, help='Path to JSON file containing extraction config info')
+parser.add_argument('-ip', '--input-path', type=str, help='Root path for elphys measurement files.')
+parser.add_argument('-op', '--output-path', type=str, help='Directory where the extraction results should be saved.')
+args = parser.parse_args()
+
+# load config JSON containing feature extraction configuration info
+with open(args.config_path) as config_json:
+    config = json.load(config_json)
+
+# set up variables in which we will collect from the protocol.txts
+protocol_types = set()  # as we iterate through cells and meaurements, unique protocol names will be collected here
+measurement_dict = {}  # this contains protocol data for each cell, for each measurement
+
+# find and iterate through input directory (each subdirectory is a cell)
+cell_directories = get_subdirectory_list(args.input_path)
+for cell in cell_directories:
+    measurement_dict[cell] = {}
+    ton, toff = None, None
+
+    # find and iterate through measurement files
+    cell_directory_path = "{}/{}".format(args.input_path, cell)
+    measurement_dirs = get_subdirectory_list(cell_directory_path)
+    for measurement_dir in measurement_dirs:
+        measurement_dir_path = "{}/{}".format(cell_directory_path, measurement_dir)
+        _, _, files = list(os.walk(measurement_dir_path))[0]
+        measurement_files = list(
+            filter(lambda name: "protocol" not in name, files))  # select only true measurement files
+
+        # read protocol file
+        with open(measurement_dir_path + '/protocol.txt') as protocol_file:
+            protocol_file_content = [line.strip("\n") for line in
+                                     protocol_file.readlines()]  # read protocol file content, strip newline characters
+        missing_swps = []
+        if len(protocol_file_content) == 5:  # when there is no missing sweep the last line is empty
+            protocol_type, channel, sampling_rate, parameters, amplitudes = protocol_file_content
+        else:
+            protocol_type, channel, sampling_rate, parameters, amplitudes, missing_swps = protocol_file_content
+
+        # calculate BPE-compatible values
+        dt = 1. / int(sampling_rate) * 1e3
+        ton, delta_t, _ = [int(p) for p in parameters.split()]
+        toff = ton + delta_t
+        amplitudes = [int(a) / 1000 for a in amplitudes.split()]  # division by 1000 to convert from pA to nA
+
+        # collect unique protocols for grouping later
+        protocol_types.add(protocol_type)
+
+        # select file containing the desired channel data as described in the protocol
+        try:
+            channel_meas_file = list(filter(lambda name: channel in name, measurement_files))[0]
+        except IndexError:  # channel name might contain double digits in filename
+            channel_name_double_digit = channel[:2] + '0' + channel[2:]
+            channel_meas_file = list(filter(lambda name: channel_name_double_digit in name, measurement_files))[0]
+        channel_meas_file, _, _ = channel_meas_file.partition(".")  # extract part before the file extension
+        channel_meas_file = "{}/{}".format(measurement_dir, channel_meas_file)  # to help BPE find the data txt
+
+        # process missing sweep indices
+        if missing_swps:
+            missing_swps_indices = list(map(lambda idx: int(idx) - 1, missing_swps.split()))  # format sweep indices
+            missing_swps = [amplitudes[idx] for idx in missing_swps_indices]
+
+        # collect protocol.txt data into measurement_dict
+        measurement_dict[cell][measurement_dir] = {
+            "protocol_type": protocol_type,
+            "measurement_file": channel_meas_file,
+            "amplitudes": amplitudes,
+            "missing_swps": missing_swps,
+            "ton": ton,
+            "toff": toff,
+            "dt": dt
+        }
+
+# create BPE-compatible config dict, grouped by protocol
+# then run BPE for each protocol type
+for protocol_type in protocol_types:
+    print_protocol_type(protocol_type)
+    cells_filtered = filter_measurements_by_protocol(measurement_dict, protocol_type)
+
+    # create a new config dict that will become the input to BluePyEFe
+    BPE_config = {
+        'path': args.input_path,
+        'features': config['features'],
+        'format': config['format'],
+        'comment': config['comment'],
+        'options': config['options'],
+        'cells': {}
+    }
+
+    print("Cells:")
+    for cell in cells_filtered:
+        print(" * " + cell)
+
+        # these variables have the same value along the measurement protocol txts (for a given cell)
+        amplitudes = []
+        ton, toff, dt = None, None, None
+
+        # these variables are meant to change depending on the measurement protocol txt content
+        files = []
+        exclude = []
+
+        for measurement in measurement_dict[cell]:
+            measurement_protocol = measurement_dict[cell][
+                measurement]  # this is the content of the protocol.txt belonging to this specific cell and measurement
+
+            # these are pretty much read once and then get overwritten by the same value
+            amplitudes = measurement_protocol['amplitudes']
+            ton, toff, dt = measurement_protocol['ton'], measurement_protocol['toff'], measurement_protocol['dt']
+
+            # these lists are expanded as we read out from the protocol.txts
+            files.append(measurement_protocol['measurement_file'])
+            missing_swps = []
+            for missing_swp in measurement_protocol['missing_swps']:
+                missing_swps.append(missing_swp)
+            exclude.append(missing_swps)
+
+        BPE_config['cells'][cell] = {
+            'v_corr': config['protocol']['v_corr'],
+            'ljp': config['protocol']['ljp'],
+            'experiments': {
+                'step': {
+                    'location': config['protocol']['location'],
+                    'files': files,
+                    'dt': dt,
+                    'amplitudes': amplitudes,
+                    'hypamp': config['protocol']['hypamp'],
+                    'ton': ton,
+                    'toff': toff
+                }
+            },
+            'exclude': exclude,
+            'exclude_unit': [[config['options']['target_unit']] for exc in range(len(exclude))]
+        }
+
+        # add further missing keys
+        BPE_config['options']['target'] = amplitudes
+
+    # run feature extraction
+    output_path_protocol = "{}/{}".format(args.output_path, protocol_type)
+    extractor = bpefe.Extractor(output_path_protocol, BPE_config)
+    extractor.create_dataset()
+    extractor.create_metadataset()
+    extractor.plt_traces()
+    extractor.extract_features()
+    try:
+        extractor.collect_global_features()
+    except Exception:
+        logging.warning("This version of BluePyEfe does not contain global feature collection. Skipping.")
+    extractor.mean_features()
+    extractor.plt_features()
+
+    extractor.feature_config_cells()
+    extractor.feature_config_all()
+
+# close log file
+sys.stdout.close()
